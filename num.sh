8db53038c7945cb61f0b97bd3df5cc8fb13bd482
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-04 04:37:21 +0200

    Add script stop and start

--- num.sh
+++ num.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script start or stop numeric services
+# F4HWN Armel
+# Usage : num.sh [stop|start|status]
+
+
 SERVICES=("ambeserver" "analog_bridge" "ircddbgateway" "md380-emu" "mmdvm_bridge" "nxdngateway" "p25gateway" "ysfgateway")
 for SERVICE in "${SERVICES[@]}"
 do
@@ -11,6 +16,10 @@ do
         stop)
             echo "Stop $SERVICE.service"
             systemctl stop $SERVICE.service
+            ;;
+        status)
+            echo "Status $SERVICE.service"
+            systemctl is-active $SERVICE.service
             ;;
         esac
 done

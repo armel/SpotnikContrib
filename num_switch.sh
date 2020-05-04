e7de3aa9484959af1b5caa16920e76aec9bcb18c
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-04 02:40:51 +0200

    Fix bug

--- num_switch.sh
+++ num_switch.sh
@@ -1,34 +1,19 @@
 #!/bin/sh
 
 #
-# Start and Stop Num Services by F4HWN Armel
+# Start or Stop Num Services by F4HWN Armel
 #
 
-case "$1" in
-    start)
-        echo "Start Num Services"
-        systemctl start ambeserver.service
-        systemctl start analog_bridge.service
-        systemctl start hblink3.service
-        systemctl start hbmonitor.service
-        systemctl start ircddbgateway.service
-        systemctl start md380-emu.service
-        systemctl start mmdvm_bridge.service
-        systemctl start nxdngateway.service
-        systemctl start p25gateway.service
-        systemctl start ysfgateway.service
-        ;;
-    stop)
-        echo "Stop Num Services"
-        systemctl stop ambeserver.service
-        systemctl stop analog_bridge.service
-        systemctl stop hblink3.service
-        systemctl stop hbmonitor.service
-        systemctl stop ircddbgateway.service
-        systemctl stop md380-emu.service
-        systemctl stop mmdvm_bridge.service
-        systemctl stop nxdngateway.service
-        systemctl stop p25gateway.service
-        systemctl stop ysfgateway.service
-        ;;
-    esac
+SERVICES=("ambeserver" "analog_bridge" "hblink3" "hbmonitor" "ircddbgateway" "md380-emu" "mmdvm_bridge" "nxdngateway" "p25gateway" "ysfgateway")
+for SERVICE in "${SERVICES[@]}"
+do
+    echo "$SERVICE.service"
+    STATUS="$(systemctl is-active $SERVICE.service)"
+    if [ "${STATUS}" = "active" ]; then
+        echo "Stop $SERVICE.service"
+        systemctl stop $SERVICE.service
+    else
+        echo "Start $SERVICE.service"
+        systemctl start $SERVICE.service
+    fi
+done

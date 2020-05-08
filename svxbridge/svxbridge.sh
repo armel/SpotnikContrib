5b702a26ac3f26521122367166f116b25b7443c4
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-09 01:06:31 +0200

    Add log

--- svxbridge/svxbridge.sh
+++ svxbridge/svxbridge.sh
@@ -5,5 +5,5 @@
 /opt/Analog_Bridge/dvswitch.sh ambemode YSFW
 # a tester manuellement et adapter les ports et les output input device 
 # si quelqu'un parle sur la ROOM ca ne marche pas donc a relancer a la main
-python3 /opt/svxbridge/svxbridge.py> /dev/null 2>&1 &
+python3 /opt/svxbridge/svxbridge.py > /tmp/svxbridge.log 2>&1 &
 

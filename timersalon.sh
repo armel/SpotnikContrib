343c23d8b45b173512b7aa323a5025f3a5eb50e7
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-05-10 17:21:16 +0200

    Improve timersalon.sh

--- timersalon.sh
+++ timersalon.sh
@@ -10,23 +10,60 @@ timeout=120
 
 # Init other values (don't touch)
 
-start=`date +%s`
+last=`date +%s`
 timer=0
+talker_start=0
+talker_stop=`date +%s`
+
+# Remove last log
+
+rm /tmp/timersalon.log
 
 # Main loop
 
 while [ $timer -lt $timeout ]; do
-    last=`grep 'ReflectorLogic: Talker stop:' /tmp/svxlink.log | tail -1 | cut -c1-24`
-    last=`date -d "$last" +%s`
+
+    # Catch last Talker start (if exist)
+
+    tmp=`grep 'ReflectorLogic: Talker start:' /tmp/svxlink.log | tail -1 | cut -c1-24`
+    if [ ! -z "$tmp" ]
+    then
+        talker_start=`date -d "$tmp" +%s`
+    fi
+
+    # Catch last Talker stop (if exist)
+
+    tmp=`grep 'ReflectorLogic: Talker stop:' /tmp/svxlink.log | tail -1 | cut -c1-24`
+    if [ ! -z "$tmp" ]
+    then
+        talker_stop=`date -d "$tmp" +%s`
+    fi
+
+    # If last Talker start > last Talker stop, then somebody is speaking so
+    #   last activity is now
+    # Else
+    #   last activity was at last Talker stop... 
+
+    if [ $talker_start -gt $talker_stop ]
+    then
+        last=`date +%s`
+    else
+        last=$talker_stop
+    fi
 
     now=`date +%s`
 
-    if [ $start -gt $last ]
-    then
-        timer=$(($now-$start))
-    else
-        timer=$(($now-$last))
-    fi
+    timer=$(($now-$last))
+
+    # Write trace for debug
+
+    echo "Last Talker Start\t: `date -d @$talker_start` ($talker_start)" >> /tmp/timersalon.log
+    echo "Last Talker Stop\t: `date -d @$talker_stop` ($talker_stop)" >> /tmp/timersalon.log
+    echo "Last Radio Activity\t: `date -d @$last` ($last)" >> /tmp/timersalon.log
+    echo "Timer : $timer seconds" >> /tmp/timersalon.log
+    echo "----------" >> /tmp/timersalon.log
+
+    # And standby...
 
     sleep 10
 done

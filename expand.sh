7c5f5a588ac2c81428b1d8938abb4fb2af8a571b
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-29 14:46:32 +0100

    Add expand at boot

--- expand.sh
+++ expand.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Expand filesystem at first boot
-/etc/init.d/resize2fs start; reboot
-# And delete me
+/etc/init.d/resize2fs start
+# Delete me
+echo "Expand done" > /tmp/foo
 rm $0
+# And finaly reboot
+/sbin/reboot

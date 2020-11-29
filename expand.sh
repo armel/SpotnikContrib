52bdd0a8ccfd186a5284ff2c69334668161cce02
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2020-11-29 15:25:24 +0100

    Add expand at boot

--- expand.sh
+++ expand.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
-NOW=$(date +"%d/%m/%Y %H%M%S")
+#!/bin/sh
+NOW=$(/bin/date +"%d/%m/%Y %H:%M:%S")
 # Expand filesystem at first boot
 /etc/init.d/resize2fs start
 # Add trace log
-echo "${NOW} Expand SD card is done" > /var/log/expand.log
+/bin/echo "${NOW} Expand SD card is done" > /var/log/expand.log
 # Delete me
-rm $0
+rm /etc/init.d/expand.sh
 # And finaly reboot
 /sbin/reboot

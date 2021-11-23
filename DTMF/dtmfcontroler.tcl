26287698894194fe44d86d4b51370214188fe160
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2021-11-23 19:14:36 +0100

    Fix bug

--- DTMF/dtmfcontroler.tcl
+++ DTMF/dtmfcontroler.tcl
@@ -9,11 +9,13 @@
   if {$cmd == "999"} {
     if { [file exists /tmp/DTMF] } {
       file delete -force /tmp/DTMF
+      playSilence 1500
       playFile /usr/share/svxlink/sounds/fr_FR/RRF/dtmf_on.wav
     } else {
       set outfile [open "/tmp/DTMF" w]
       puts $outfile "DTMF OFF"
       close $outfile
+      playSilence 1500
       playFile /usr/share/svxlink/sounds/fr_FR/RRF/dtmf_off.wav
     }
     return 1
--- DTMF/timersalon.tcl
+++ DTMF/timersalon.tcl
@@ -3,12 +3,14 @@
     if { [file exists /tmp/TIMER] } {
       file delete -force /tmp/TIMER
       exec nohup /etc/spotnik/timersalon.sh &
+      playSilence 1500
       playFile /usr/share/svxlink/sounds/fr_FR/RRF/timer_on.wav
     } else {
       set outfile [open "/tmp/TIMER" w]
       puts $outfile "TIMER OFF"
       close $outfile
       exec /usr/bin/pkill -f timersalon
+      playSilence 1500
       playFile /usr/share/svxlink/sounds/fr_FR/RRF/timer_off.wav
     }
     return 1
